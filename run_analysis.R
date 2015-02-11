44849d40727aaf752ab480e5852193c687899442
alejarbu <alejarbu@gmail.com>
2015-02-11 13:00:46 -0500

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,101 +1,75 @@
 library(tidyr)
 library(dplyr)
 library(data.table)
+library(reshape2)
 
-#############################
-# LOAD SUBJECT
-#############################
+############## Functions
 
-file<-"D:\\UCI HAR Dataset\\train\\subject_train.txt"
-subject_train <- fread(file)
-setnames(subject_train,1,"subject")
-
-file<-"D:\\UCI HAR Dataset\\test\\subject_test.txt"
-subject_test <- fread(file)
-setnames(subject_test,1,"subject")
-
-subject_data<-rbind(subject_train,subject_test)
-remove("subject_train")
-remove("subject_test")
-
-#############################
-# LOAD Y
-#############################
-
-file<-"D:\\UCI HAR Dataset\\train\\y_train.txt"
-y_train <- fread(file)
-setnames(y_train,1,"activity")
-
-file<-"D:\\UCI HAR Dataset\\test\\y_test.txt"
-y_test <- fread(file)
-setnames(y_test,1,"activity")
-
-y_data<-rbind(y_train,y_test)
-remove("y_train")
-remove("y_test")
-
-#############################
-# LOAD X
-#############################
-
-file<-"D:\\UCI HAR Dataset\\train\\X_train.txt"
-#x_train <- fread(file) #Not working
-x_train <- data.table(read.table(file))
-
-file<-"D:\\UCI HAR Dataset\\test\\X_test.txt"
-x_test <- data.table(read.table(file))
-
-file<-"D:\\UCI HAR Dataset\\features.txt"
-features <- fread(file)
-setnames(x_train,1:561,features$V2)
-setnames(x_test,1:561,features$V2)
-remove("features")
-
-x_data<-rbind(x_train,x_test)
-remove("x_train")
-remove("x_test")
-
-#############################
 # Merges the training and the test sets to create one data set And
 # Extracts only the measurements on the mean and standard 
 # deviation for each measurement. 
-#############################
+getFilteredDataToAnalysis<-function(){
+  # Load subject  
+  subject_train <- fread("UCI HAR Dataset/train/subject_train.txt")
+  subject_test <- fread("UCI HAR Dataset/test/subject_test.txt")
+  subject_data<-rbind(subject_train,subject_test)
+  setnames(subject_data,1,"subject")
+  
+  # Load Y  
+  y_train <- fread("UCI HAR Dataset/train/y_train.txt")
+  y_test <- fread("UCI HAR Dataset/test/y_test.txt")
+  y_data<-rbind(y_train,y_test)
+  setnames(y_data,1,"activity")
+  
+  # Load X
+  ###x_train <- fread(file) #Not working
+  x_train <- data.table(read.table("UCI HAR Dataset/train/X_train.txt"))
+  x_test <- data.table(read.table("UCI HAR Dataset/test/X_test.txt"))
+  
+  features <- fread("UCI HAR Dataset/features.txt")
+  x_data<-rbind(x_train,x_test)
+  setnames(x_data,1:561,features$V2)
+  
+  # Merges the sets and Reduce variables
+  filtered_data<-cbind(select(x_data,contains("mean")),
+  select(x_data,contains("std")))
+  filtered_data<-select(filtered_data,-contains("meanFreq"))
+  filtered_data<-select(filtered_data,-contains("angle"))
+  
+  cbind(subject_data,y_data,filtered_data)
+}
 
-filtered_data<-cbind(select(x_data,contains("mean")),
-select(x_data,contains("std")))
-filtered_data<-select(filtered_data,-contains("meanFreq"))
-filtered_data<-select(filtered_data,-contains("angle"))
-
-final_data<-cbind(subject_data,y_data,filtered_data)
-
-#############################
-# Uses descriptive activity names to name the activities 
-# in the data set
-#############################
-
-activities<-c('WALKING',
-              'WALKING_UPSTAIRS',
-              'WALKING_DOWNSTAIRS',
-              'SITTING',
-              'STANDING',
-              'LAYING')
-
-final_data<-mutate(final_data,activity=activities[activity])
-
-#############################
 # Appropriately labels the data set with descriptive variable names
-#############################
+renameColumns<-function(data){
+  replacements <-list(c("-mean()","Mean"),
+                      c("-std()","Std"),
+                      c("BodyBody","Body")
+  )
+  tidy_names <- colnames(data)
+  for(r in replacements){
+    tidy_names <- gsub(r[1],r[2],tidy_names,fixed=TRUE) 
+  }
+  colnames(data) <- tidy_names
+}
 
 
-#############################
-# From the data set in step 4, creates a second, independent
-# tidy data set with the average of each variable for each activity
-# and each subject.
-#############################
+############## Start script
 
-final_data[,mean('tBodyAcc-mean()-X',na.rm = TRUE),by=list(activity,subject)]
+# Get the data
+filteredData<-getFilteredDataToAnalysis()
 
-final_data %>%
-  group_by(activity,subject) %>%
-  summarize(mean('tBodyAcc-mean()-X',na.rm = TRUE)) %>%
-  print
+# Uses descriptive activity names to name the activities in the data set
+activities <- read.table("UCI HAR Dataset/activity_labels.txt")[,"V2"]
+filteredData <- filteredData %>% mutate(activity=activities[activity])
+
+# tidy vars names
+renameColumns(filteredData)
+
+#Write file
+write.csv(filteredData, "finaldata.txt", row.names=FALSE)
+
+# Data set with the average of each variable for each activity and each subject.
+filteredData %>%
+  gather(variable, value, -c(1,2)) %>%
+  dcast(activity + subject ~ variable, mean) %>%
+  write.csv("finaldata_summarized.txt", row.names=FALSE)
